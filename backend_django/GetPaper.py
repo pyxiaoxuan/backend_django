6b2f00c11d5da19f3e99e86d70537ecda5f2b16b
wtx <wtx970722@163.com>
2018-12-24 19:52:56 +0800

    添加GetPaper函数

---
+++ backend_django/GetPaper.py
@@ -0,0 +1,23 @@
+from .SqlModule import addSql,modSql,delSql
+from __future__ import division
+import random
+timestamp = 0
+
+def getpaper(n,k): #total number, threshold
+    global timestamp
+    timestamp += 1
+
+    paper = []
+    j = 0 #duplicate count
+    for _ in range(1,n):
+        p = (k-j)/n
+        print(p)
+        r = random.random()
+        if r < p :
+            #paper.append(qryDupQuestion(timestamp))  TODO
+            j += 1
+        else :
+            j += 0 #避免注释下一行之后报错
+            #paper.append(qryNewQuestion(timestamp))  TODO
+    assert j<=k
+    return paper
