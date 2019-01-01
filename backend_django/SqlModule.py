2730c27b5d62386f8fdf520ff3cf9541ba42f3e2
rfnoah <412983376@qq.com>
2019-01-01 15:56:07 +0800

    增删改查完善

--- backend_django/SqlModule.py
+++ backend_django/SqlModule.py
@@ -1,16 +1,16 @@
 from django.db import models
 from .models import Question
-def addSql(_QID,_Body,_Unit,_Old,_Number):
+def addSql(_QID,_Body,_Unit,_Old,_Number):  #增
     Q = Question(QID=_QID,Body=_Body,Unit=_Unit,Old=_Old,Number=_Number)
     Q.save()
-    Question.objects.all()
+    Question.objects.all()      #调试用
 
-def delSql(_QID):
+def delSql(_QID):                           #删
     Q = Question.objects.get(QID=_QID)
     Q.delete()
     Question.objects.all()
 
-def modSql(_QID,_Body,_Unit,_Old,_Number):
+def modSql(_QID,_Body,_Unit,_Old,_Number):  #改
     Q = Question.objects.get(QID=_QID)
     Q.Body=_Body
     Q.Unit=_Unit
@@ -18,6 +18,12 @@ def modSql(_QID,_Body,_Unit,_Old,_Number):
     Q.Number=_Number
     Q.save()
     Question.objects.all()
+
+def srhSql(_QID=-1):                             #查
+    if _QID==-1:                                #若缺省则随机查找
+        return Question.objects.get(random.randint(0,Question.objects.count()))
+    else:                                       #否则按照Id查找
+        return Question.objects.get(QID=_QID)
 
 
 
